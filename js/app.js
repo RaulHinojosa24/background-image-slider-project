fe5e6288a802ad725971c9178cd6adaac29f3ef5
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-03-09 13:57:02 +0100

    v1.0

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
-  const pictures = [
-    "contBcg-0",
-    "contBcg-1",
-    "contBcg-2",
-    "contBcg-3",
-    "contBcg-4"
-  ];
+const pictures = [
+  "contBcg-0",
+  "contBcg-1",
+  "contBcg-2",
+  "contBcg-3",
+  "contBcg-4"
+];
+
+let index = 0;
+
+document.querySelector(".img-container").style.backgroundImage = `url(../img/${pictures[0]}.jpeg)`;
+
+document.querySelector(".btn-left").addEventListener("click", function () {
+  slide(-1);
+});
+document.querySelector(".btn-right").addEventListener("click", function () {
+  slide(1);
+});
+
+function slide(direction) {
+  if (pictures[index + direction] !== undefined) {
+    const img = document.querySelector(".img-container");
+    index += direction;
+    img.style.backgroundImage = `url(../img/${pictures[index]}.jpeg)`
+  }
+}
